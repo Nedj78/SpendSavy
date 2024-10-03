776f74e666bb5c7332f3a9d64f88e4e5fd77aaf5
Nedj <106325998+Nedj78@users.noreply.github.com>
2024-10-03 17:25:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -290,7 +290,6 @@ const financesExpense = document.querySelector('.section12');
 const otherExpense = document.querySelector('.section13');
 
 function hideEmploymentIncome {
-    employmentIncome.classList.add('visible');
     employmentIncome.classList.toggle('hidden');
 }
 employmentIncome.addEventListener('click', (hideEmploymentIncome);
