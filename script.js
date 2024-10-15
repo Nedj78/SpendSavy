083f9020940434cd9e472fa8a913f5c9e2e4addb
Nedj <106325998+Nedj78@users.noreply.github.com>
2024-10-16 00:39:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -389,15 +389,15 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         totalIncome = 0;
-        totalIncomeElement.innerText = totalIncome.toFixed(2) + " " + "$";
+        totalIncomeElement.innerText =  "$" + " " + totalIncome.toFixed(2);
         totalIncomeElement.style.color = 'black';
 
         let totalExpenses = 0;
-        totalExpensesElement.innerText = totalExpenses.toFixed(2) + " " + "$";
+        totalExpensesElement.innerText =  "$" + " " + totalExpenses.toFixed(2);
         totalExpensesElement.style.color = 'black';
 
         balance = 0;
-        balanceElement.innerText = balance.toFixed(2) + " " + "$";
+        balanceElement.innerText = "$" + " " + balance.toFixed(2);
         balanceElement.style.color = 'black';
     });
 });
